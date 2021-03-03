97b0afd1680c70f8639e633f41b1f541a0f76d2c
kristofajosh <christopherjoshua18@live.com>
2021-03-03 19:32:25 +0100

    fix dropdown

--- src/web/components/composite/header/index.tsx
+++ src/web/components/composite/header/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.styles.css';
 import { MENUS } from './menus';
 import { GiHamburgerMenu, MdKeyboardArrowDown } from 'react-icons/all';
 
 const Header = () => {
+    const [dropView, setDropView] = useState([]);
     const showDrops = (drops: any) => {
-        console.log(drops);
+        drops && setDropView(drops);
     };
+
     return (
         <div className={'header'}>
             <div className="header__content nav-container">
@@ -14,9 +16,13 @@ const Header = () => {
                     <img src="/logo.svg" alt="logo" />
                 </div>
                 <div className="header__content_nav--menus">
-                    {MENUS.map((el) => (
-                        <div className="header__content_nav--menus_menu" onMouseEnter={() => showDrops(el?.drops)}>
-                            <span className={'menu_title'}>{el.title}</span>
+                    {MENUS.map((el, i) => (
+                        <div
+                            className="header__content_nav--menus_menu"
+                            onMouseEnter={() => showDrops(el?.drops)}
+
+                            key={i}>
+                            <span onClick={()=> showDrops(!el?.drops)} className={'menu_title'}>{el.title}</span>
                             {el?.drops && (
                                 <span className={'menu_title--has-more'}>
                                     <MdKeyboardArrowDown size={22} />
@@ -25,24 +31,26 @@ const Header = () => {
                         </div>
                     ))}
                 </div>
-                {/*<div className={'menu_dropdown'}>*/}
-                {/*    <div className={'menu_dropdown--options nav-container'}>*/}
-                {/*        {['drps'].map((el) => (*/}
-                {/*            <div className={'drop-item'}>*/}
-                {/*                <p>{el}</p>*/}
-                {/*                <p>*/}
-                {/*                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque libero neque quidem.*/}
-                {/*                </p>*/}
-                {/*            </div>*/}
-                {/*        ))}*/}
-                {/*    </div>*/}
-                {/*    <div className={'menu_dropdown_footer'}>*/}
-                {/*        <p>Are you a journalist or an editor?</p>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                {dropView.length ? (
+                    <div className={'menu_dropdown'} onMouseLeave={()=>showDrops([])}>
+                        <div className={'menu_dropdown--options nav-container'}>
+                            {dropView.map((el: any) => (
+                                <div className={'drop-item'}>
+                                    <p>{el.title}</p>
+                                    <p>
+                                        {el.description}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className={'menu_dropdown_footer'}>
+                            <p>Are you a journalist or an editor?</p>
+                        </div>
+                    </div>
+                ): null}
                 <div className="header__content__mobile-nav">
                     <div className={'hamburger'}>
-                        <GiHamburgerMenu size={18} color={'white'}/>
+                        <GiHamburgerMenu size={18} color={'white'} />
                     </div>
                 </div>
             </div>
