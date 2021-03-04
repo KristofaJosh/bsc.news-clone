8a54d79700f55d656bdf8e4346ea346006618855
kristofajosh <christopherjoshua18@live.com>
2021-03-04 11:11:00 +0100

    header dropdown wasn't showing correctly

--- src/web/components/composite/header/index.tsx
+++ src/web/components/composite/header/index.tsx
@@ -6,12 +6,12 @@ import { GiHamburgerMenu, MdKeyboardArrowDown } from 'react-icons/all';
 const Header = () => {
     const [dropView, setDropView] = useState([]);
     const showDrops = (drops: any) => {
-        drops && setDropView(drops);
+        drops ? setDropView(drops) : setDropView([]);
     };
 
     return (
         <div className={'header'}>
-            <div className="header__content nav-container">
+            <div className="header__content nav-container" >
                 <div className="header__content_logo">
                     <img src="/logo.svg" alt="logo" />
                 </div>
@@ -20,9 +20,8 @@ const Header = () => {
                         <div
                             className="header__content_nav--menus_menu"
                             onMouseEnter={() => showDrops(el?.drops)}
-
                             key={i}>
-                            <span onClick={()=> showDrops(!el?.drops)} className={'menu_title'}>{el.title}</span>
+                            <span className={'menu_title'}>{el.title}</span>
                             {el?.drops && (
                                 <span className={'menu_title--has-more'}>
                                     <MdKeyboardArrowDown size={22} />
